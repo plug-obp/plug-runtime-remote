802bac2f21b1e9fb3ea15d40c317e2fc843cbd66
Jean-Charles <jeancharles.roger@me.com>
2018-01-15 11:24:48 +0100

    Adds support to run a command for a remote runtime.

--- src/plug/language/remote/RemoteDescription.java
+++ src/plug/language/remote/RemoteDescription.java
@@ -4,29 +4,42 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RemoteDescription {
 
+	// command to execute for a distant runtime
+	private final String[] command;
+	// delay to wait before connecting
+	private final int delay;
+
+	// host of the distant runtime
 	private final String host;
-	private final String command;
+	// port to connect to the runtime
 	private final int port;
 
 	public RemoteDescription(
+			@JsonProperty("command")
+			String[] command,
+			@JsonProperty("delay")
+			int delay,
 			@JsonProperty("host")
 			String host,
-			@JsonProperty("command")
-			String command,
 			@JsonProperty("port")
 			int port)
 	{
-		this.host = host;
 		this.command = command;
+		this.delay = delay;
+		this.host = host;
 		this.port = port;
+	}
+
+	public String[] getCommand() {
+		return command;
+	}
+
+	public int getDelay() {
+		return delay;
 	}
 
 	public String getHost() {
 		return host;
-	}
-
-	public String getCommand() {
-		return command;
 	}
 
 	public int getPort() {
--- src/plug/language/remote/RemoteLoader.java
+++ src/plug/language/remote/RemoteLoader.java
@@ -2,11 +2,14 @@ package plug.language.remote;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
+import java.nio.file.Paths;
 import java.util.Map;
 import plug.core.ILanguageLoader;
 import plug.core.ILanguageRuntime;
 import plug.language.remote.runtime.RemoteRuntime;
+import plug.utils.exec.ProcessRunner;
 
 public class RemoteLoader implements ILanguageLoader {
 
@@ -18,7 +21,27 @@ public class RemoteLoader implements ILanguageLoader {
 	public ILanguageRuntime getRuntime(URI modelURI, Map<String, Object> options) {
 		try {
 			RemoteDescription description = new ObjectMapper().readValue(modelURI.toURL(), RemoteDescription.class);
-			RemoteRuntime runtime = new RemoteRuntime(description.getHost(), description.getPort());
+
+			String[] command = description.getCommand();
+			String host = description.getHost();
+			if (command != null && command.length > 0) {
+				// starts the process
+				ProcessRunner runner = new ProcessRunner(new PrintWriter(System.out), new PrintWriter(System.err));
+				runner.setWorkingPath(Paths.get(modelURI.getPath()).getParent());
+				runner.startProcess(command, null);
+
+				// sets the host to localhost
+				host = "localhost";
+
+				// waits (and yield) to allow the command to listen to the connection.
+				try {
+					Thread.sleep(description.getDelay());
+				} catch (InterruptedException e) {
+					// nothing to do
+				}
+			}
+
+			RemoteRuntime runtime = new RemoteRuntime(host, description.getPort());
 			// TODO check when the runtime should be initialized
 			runtime.initializeRuntime();
 			return runtime;
